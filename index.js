ea73b98d02628a40771adf5c9dc1f55697e132a6
Aeris One <aeris@e.email>
2021-01-31 16:02:44 +0100

    Débug 3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 const expired = {
-  run: () => {
+  run: (date) => {
     const now = new Date();
     if(months.slice(9-now.getMonth(), 12-now.getMonth())[0] >= date.getMonth() && date.getFullYear() == now.getFullYear()) {
       return false;
