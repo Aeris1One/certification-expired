3da8effbcb9665894a46e51b98d19dfe362cc530
Aeris One <aeris@e.email>
2021-01-31 16:00:27 +0100

    C'est moi ou je suis stupide ? -_-

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-const drive_usage = {
+const expired = {
   run: () => {
     const now = new Date();
     if(months.slice(9-now.getMonth(), 12-now.getMonth())[0] >= date.getMonth() && date.getFullYear() == now.getFullYear()) {
