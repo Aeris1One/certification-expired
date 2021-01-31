71ec49a04d677372602a95da3177e6fad790e999
Aeris One <aeris@e.email>
2021-01-31 15:50:11 +0100

    Retrait de l'async, enfin tentative de retrait...

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 const expired = {
-  run: async () => {
+  run: () => {
     var now = new Date();
     if(months.slice(9-now.getMonth(), 12-now.getMonth())[0] >= date.getMonth() && date.getFullYear() == now.getFullYear()) {
       return false
@@ -11,7 +11,7 @@ const expired = {
 }
   },
   description: "Is certification expired ?",
-  isAsync: true,
+  isAsync: false,
   arguments: [{ name: "date", type: "Date" }],
 };
 
