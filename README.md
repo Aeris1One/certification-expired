c6a255f5c3f49bc5d6b1de02de18bba9e6055427
Aeris One <aeris@e.email>
2021-01-31 15:23:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# certified-expiration
+
+- `expired(date)` if the date provided is older than 3 months (is the certification expired)
+
+Created for TheElderGuardian Certified program.
+
---
+++ index.js
@@ -0,0 +1,21 @@
+let months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const expired = {
+  run: async () => {
+    var now = new Date();
+    if(months.slice(9-now.getMonth(), 12-now.getMonth())[0] >= date.getMonth() && date.getFullYear() == now.getFullYear()) {
+      return false
+    }
+    return true
+  }
+}
+  },
+  description: "Is certification expired ?",
+  isAsync: true,
+  arguments: [{ name: "date", type: "Date" }],
+};
+
+module.exports = {
+  sc_plugin_api_version: 1,
+  functions: { expired },
+};
